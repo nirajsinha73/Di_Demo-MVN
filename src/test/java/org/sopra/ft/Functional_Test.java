50d041213c07e6884b4c6ce91700cfc2b8b13fea
nirajsinha73 <54464397+nirajsinha73@users.noreply.github.com>
2021-10-01 00:37:16 +0530

    Update Functional_Test.java

--- src/test/java/org/sopra/ft/Functional_Test.java
+++ src/test/java/org/sopra/ft/Functional_Test.java
@@ -10,8 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
 
 public class Functional_Test {
 
@@ -19,21 +18,21 @@ public class Functional_Test {
 	public void functionalTest() throws InterruptedException {
 		
 		
-		options = Options()
-		options.headless = True
-		driver = webdriver.Chrome(options=options)
-		driver.get("https://www.youtube.com/")
-		element_text = driver.find_element_by_id("title").text
-		print(element_text)
-	//System.setProperty("webdriver.chrome.driver", "C:\\Office Work\\Software\\chromedriver.exe");
-    	//ChromeOptions options = new ChromeOptions();
+		//options = Options()
+		//options.headless = True
+		//driver = webdriver.Chrome(options=options)
+		//driver.get("https://www.youtube.com/")
+		//element_text = driver.find_element_by_id("title").text
+		//print(element_text)
+	System.setProperty("webdriver.chrome.driver", "C:\\Office Work\\Software\\chromedriver.exe");
+    	ChromeOptions options = new ChromeOptions();
         //options.addArguments("--headless");
         //options.addArguments("--disable-gpu");
-		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		// Launch website
-		//driver.navigate().to("https://www.google.com/");
+		driver.navigate().to("https://www.google.com/");
 		//driver.navigate().refresh();
-		//TimeUnit.SECONDS.sleep(30);
+		TimeUnit.SECONDS.sleep(30);
 		// Click on the search text box and send value
 		//WebElement divElement = driver.findElement(By.id("header_h1"));
 		//String str = divElement.getText();
