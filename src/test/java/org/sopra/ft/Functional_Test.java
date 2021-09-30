9e756a6bccecc1ea71d2e17ffa2b4d03934735ae
nirajsinha73 <54464397+nirajsinha73@users.noreply.github.com>
2021-10-01 00:34:18 +0530

    Update Functional_Test.java

--- src/test/java/org/sopra/ft/Functional_Test.java
+++ src/test/java/org/sopra/ft/Functional_Test.java
@@ -10,20 +10,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+from selenium import webdriver;
+from selenium.webdriver.chrome.options import Options;
 
 public class Functional_Test {
 
 	@Test
 	public void functionalTest() throws InterruptedException {
-	System.setProperty("webdriver.chrome.driver", "C:\\Office Work\\Software\\chromedriver.exe");
+		
+		
+		options = Options()
+		options.headless = True
+		driver = webdriver.Chrome(options=options)
+		driver.get("https://www.youtube.com/")
+		element_text = driver.find_element_by_id("title").text
+		print(element_text)
+	//System.setProperty("webdriver.chrome.driver", "C:\\Office Work\\Software\\chromedriver.exe");
     	//ChromeOptions options = new ChromeOptions();
         //options.addArguments("--headless");
         //options.addArguments("--disable-gpu");
-		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new ChromeDriver();
 		// Launch website
-		driver.navigate().to("https://www.google.com/");
-		driver.navigate().refresh();
-		TimeUnit.SECONDS.sleep(30);
+		//driver.navigate().to("https://www.google.com/");
+		//driver.navigate().refresh();
+		//TimeUnit.SECONDS.sleep(30);
 		// Click on the search text box and send value
 		//WebElement divElement = driver.findElement(By.id("header_h1"));
 		//String str = divElement.getText();
